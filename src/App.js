c8b08128b5c5b2cf925df4d8c2e4b361ff1bfe39
Ste <n00202054@iadt.ie>
2023-05-05 00:50:05 +0100

    small fixes

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ const App = () => {
   return (
     <>
      
+      <div className="scroll-smooth">
       <div className="bg-gradient-to-r from-[#ff4632] to-[#fd633d] sticky top-0 min-h-screen overflow-hidden  items-center flex justify-center overflow-x-auto">
         <div className='text-8xl font-bold text-white'>
           Join the band
@@ -31,7 +32,7 @@ const App = () => {
       <div className="bg-white min-h-screen flex sticky top-0 border border-[#1010181f] rounded-t-3xl">
         
         <a href='#who-are-we' className='w-full absolute'>
-          <div className='text-3xl whitespace-nowrap pl-20 h-fit p-10 text-left text-black hover:opacity-70 ease-in-out transition duration-500'>
+          <div className='text-3xl whitespace-nowrap pl-20 h-fit p-10 text-left text-black hover:opacity-70 ease-in-out transition duration-300 lg:duration-500'>
             Who are we
           </div>
         </a>
@@ -48,13 +49,13 @@ const App = () => {
       <div id="where-we-belong"></div> 
       <div className='bg-white min-h-screen w-full flex sticky top-[120px] border border-[#1010181f]  rounded-t-3xl'>
         <a href='#where-we-belong' className='w-full absolute'>
-          <div className='text-3xl whitespace-nowrap pl-20 h-fit p-10 text-left text-black hover:opacity-70 ease-in-out transition duration-500'>
+          <div className='text-3xl whitespace-nowrap pl-20 h-fit p-10 text-left text-black hover:opacity-70 ease-in-out transition duration-300 lg:duration-500'>
               Where we belong
           </div>
         </a>
         <div className='flex w-full justify-center items-center'>
           <div className={`text-5xl`}>
-          Listening and learning
+          We all have parts to play
           </div>
         </div>
       </div>
@@ -63,19 +64,24 @@ const App = () => {
 
         
       <div id="how-we-act"></div> 
-      <div className={`min-h-screen relative top-[120px] border border-[#1010181f]  transition-all ease-in-out duration-500  ${isInView == true ? 'bg-[#2d46b9] ' : 'bg-white  rounded-t-3xl' }`}>
+      <div className={`min-h-screen relative flex top-[120px] border border-[#1010181f]  transition-all ease-in-out duration-300 lg:duration-500  ${isInView == true ? 'bg-[#2d46b9] text-white' : 'bg-white text-black rounded-t-3xl' }`}>
         
-      <a href='#how-we-act' className='w-full absolute'>
-          <div className='text-3xl whitespace-nowrap pl-20 h-fit p-10 text-left text-black hover:opacity-70 ease-in-out transition duration-500'>
+        <a href='#how-we-act' className='w-full absolute'>
+          <div className='text-3xl whitespace-nowrap pl-20 h-fit p-10 text-left text-black hover:opacity-70 ease-in-out transition duration-300 lg:duration-500'>
               How we act
           </div>
         </a>
+        <div className='flex w-full justify-center items-center'>
+          <div className={`text-5xl`}>
+            Listening and learning
+          </div>
+        </div>
       
       </div>
     
-      <div className={`grid grid-cols-1 w-full min-h-screen absolute rounded-t-3xl justify-center transition-all ease-in-out duration-500 ${isInView == true ? 'bg-[#2d46b9] border-white text-white' : 'bg-white border-[#1010181f] text-black' }`}>
+      <div className={`grid grid-cols-1 w-full min-h-screen absolute rounded-t-3xl justify-center transition-all ease-in-out duration-300 lg:duration-500 ${isInView == true ? 'bg-[#2d46b9] border-white text-white' : 'bg-white border-[#1010181f] text-black' }`}>
     
-        <div className={`text-5xl flex transition-all self-center ease-in-out duration-500 justify-self-center mb-40`}>
+        <div className={`text-5xl flex transition-all self-center ease-in-out duration-300 lg:duration-500 justify-self-center mb-40`}>
           Featured Jobs
         </div>
         
@@ -84,7 +90,7 @@ const App = () => {
         </div>
 
   
-        <div className={`grid grid-cols-1 w-full justify-center min-h-screen transition-all ease-in-out duration-500  ${isInView == true ? 'bg-[#2d46b9] text-white' : 'bg-white text-black'}`}>
+        <div className={`grid grid-cols-1 w-full justify-center min-h-screen transition-all ease-in-out duration-300 lg:duration-500  ${isInView == true ? 'bg-[#2d46b9] text-white' : 'bg-white text-black'}`}>
         
           <div className={"text-5xl text-center flex self-center justify-self-center"}>
             Here, there, everywhere
@@ -93,9 +99,9 @@ const App = () => {
         </div>
 
 
-        <div className={`grid grid-cols-1 w-full justify-center text-black min-h-screen transition-all ease-in-out duration-500 ${isPinkInView == true ? 'bg-[#ffcdd2]' : 'bg-white' }`}>
+        <div className={`grid grid-cols-1 w-full justify-center text-black min-h-screen transition-all ease-in-out duration-300 lg:duration-500 ${isPinkInView == true ? 'bg-[#ffcdd2]' : 'bg-white' }`}>
         
-          <div className={"text-5xl text-center flex self-center justify-self-center transition-all ease-in-out duration-500 "}>
+          <div className={"text-5xl text-center flex self-center justify-self-center transition-all ease-in-out duration-300 lg:duration-500 "}>
             Fresh Content
           </div>
 
@@ -106,6 +112,7 @@ const App = () => {
     
         </div>
       
+      </div>
       </div>
           
           
